616007a1550806d0e669c9081c692a05c15acc89
Nik T <xml12333@gmail.com>
2023-05-05 09:06:04 +0300

    05-five-router-animated

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <img src="https://picsum.photos/800/300" alt="img">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus
+      repellendus aliquam delectus eveniet laborum.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod tempora
+      architecto quis ipsa fugiat et?
+    </p>
+  </main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, qui.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta aperiam
+      pariatur tenetur fuga error. In sit iste velit cumque harum? Ex mollitia
+      iste debitis minima.
+    </p>
+  </main>
+</template>
