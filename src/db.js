d9d9de012826a66f34255b4eae332634b5cf0ce0
Nik T <xml12333@gmail.com>
2023-05-06 16:20:50 +0300

    chat-app-and-firebase

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_apiKey,
+  authDomain: import.meta.env.VITE_APP_authDomain,
+  projectId: import.meta.env.VITE_APP_projectId,
+  storageBucket: import.meta.env.VITE_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
+  appId: import.meta.env.VITE_APP_appId,
+};
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+export {db};
